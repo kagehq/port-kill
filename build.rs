6a0113c28d02675f300f64ae79797ac7a434f034
Dante Lex <dantelex2@gmail.com>
2025-09-02 08:34:26 -0400

    Windows build: remove helper referring to winres in type position; use inline block so build compiles once winres is resolved

--- build.rs
+++ build.rs
@@ -1,22 +1,18 @@
 fn main() {
     #[cfg(target_os = "windows")]
     {
-        // Only run winres when available (on Windows builds)
         if std::path::Path::new("assets/port-kill.ico").exists() {
-            if let Ok(mut res) = try_winres() {
+            let res_build = (|| -> Result<(), Box<dyn std::error::Error>> {
+                let mut res = winres::WindowsResource::new();
                 res.set_icon("assets/port-kill.ico");
-                let _ = res.compile();
+                res.compile()?;
+                Ok(())
+            })();
+            if let Err(e) = res_build {
+                println!("cargo:warning=winres failed: {}", e);
             }
         }
     }
 }
-
-#[cfg(target_os = "windows")]
-fn try_winres() -> Result<winres::WindowsResource, ()> {
-    Ok(winres::WindowsResource::new())
-}
-
-#[cfg(not(target_os = "windows"))]
-fn try_winres() -> Result<(), ()> { Err(()) }
 
 
