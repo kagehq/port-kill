000fb67bc2cc8bd3b442fc107c4f9b5008bd4dfa
Dante Lex <dantelex2@gmail.com>
2025-09-27 23:19:07 -0400

    Fix Nix version compatibility - use nixos-24.05
    - Change from nixos-unstable to nixos-24.05
    - nixos-unstable requires Nix 2.18+ but GitHub Actions has 2.16.1
    - nixos-24.05 is compatible with Nix 2.16.1 and has newer Rust
    - Should resolve both Nix version and Rust version issues
    
    This should finally fix the Nix build compatibility issues.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Port Kill - A CLI tool to help you find and free ports blocking your dev work";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
